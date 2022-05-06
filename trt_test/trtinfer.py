15738431773b562637ef13a7c3d9e90059d91763
Wang Yumeng <wangyumeng02@megvii.com>
2022-05-06 11:27:02 +0800

    trtinfer improved

--- trt_test/trtinfer.py
+++ trt_test/trtinfer.py
@@ -11,6 +11,8 @@ class TRTInference(object):
         engine_data = open(model_file, 'rb').read()
         self.trt_engine = self.trt_runtime.deserialize_cuda_engine(engine_data)
         self.context = self.trt_engine.create_execution_context()
+        self.input_buffers = {}
+        self.output_buffers = {}
 
         # for i in range(self.trt_engine.num_bindings):
         #     bname = self.trt_engine.get_binding_name(i)
@@ -18,7 +20,7 @@ class TRTInference(object):
         #     print(bname, bshape)
 
     def inference(self, inputs, stream):
-        self.bindings = []
+        bindings = []
         device = torch.device("cuda")
         for i in range(self.trt_engine.num_bindings):
             bname = self.trt_engine.get_binding_name(i)
@@ -31,12 +33,12 @@ class TRTInference(object):
                 else:
                     self.input_buffers[bname] = inputs[bname].to(device,
                         memory_format=torch.contiguous_format)
-                self.bindings.append(self.input_buffers[bname].data_ptr())
+                bindings.append(self.input_buffers[bname].data_ptr())
             else:
                 if bname not in self.output_buffers:
                     self.output_buffers[bname] = torch.empty(size=list(bshape),
                         dtype=torch.float32, device=device).contiguous()
-                self.bindings.append(self.output_buffers[bname].data_ptr())
-        self.context.execute_async_v2(self.bindings, stream.cuda_stream)
+                bindings.append(self.output_buffers[bname].data_ptr())
+        self.context.execute_async_v2(bindings, stream.cuda_stream)
         return self.output_buffers
 
